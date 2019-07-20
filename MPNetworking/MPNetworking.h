ccbc5c6d7df317b1286308a9259b4260b2e53f48
Kutan Çıngısız <kutan.cingisiz@barakatech.com>
2019-07-20 13:22:08 +0300

    Initial Commit

---
+++ MPNetworking/MPNetworking.h
@@ -0,0 +1,19 @@
+//
+//  MPNetworking.h
+//  MPNetworking
+//
+//  Created by KUTAN ÇINGISIZ on 20.07.2019.
+//  Copyright © 2019 KUTAN ÇINGISIZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MPNetworking.
+FOUNDATION_EXPORT double MPNetworkingVersionNumber;
+
+//! Project version string for MPNetworking.
+FOUNDATION_EXPORT const unsigned char MPNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MPNetworking/PublicHeader.h>
+
+
